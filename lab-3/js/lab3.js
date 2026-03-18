5de277c3bd1c2a9c79696e49a0b883b6d0c6c742
achlles26 <200628218@student.georgianc.on.ca>
2026-03-18 17:19:47 -0400

    wrote up css and javascript

---
+++ lab-3/js/lab3.js
@@ -0,0 +1,23 @@
+const lis = document.querySelectorAll('li[role="tab"]');
+const divs = document.querySelectorAll('div[role="tabpanel"]')
+
+divs.forEach(div =>{
+
+    div.style.display = "none";
+});
+
+
+lis.forEach((li) =>{
+
+    li.addEventListener("click", (event)=>{
+
+        divs.forEach(div =>{
+
+            div.style.display = "none";
+        });
+
+        let div = document.getElementById(li.querySelector("a").href.slice(66))
+        div.style.display = "block";
+    });
+
+});
